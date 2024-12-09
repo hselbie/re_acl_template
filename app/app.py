53b5d81051a66ec2b916a10b2ddcc78fcada80b2
hugoselbie <hugoselbie@google.com>
2024-12-09 16:22:13 +0000

    working remote deployment

--- app/app.py
+++ app/app.py
@@ -1,8 +1,7 @@
 from agents import agent_structure, agent_holder
 import tomllib
 from vertexai.preview import reasoning_engines
-
-
+import vertexai
 
 with open("config.toml", "rb") as f:
     config = tomllib.load(f)
@@ -10,14 +9,20 @@ with open("config.toml", "rb") as f:
 my_model = config['AGENT_DEFAULT']['model']
 my_project = config['AGENT_DEFAULT']['project_id']
 my_location = config['AGENT_DEFAULT']['location']
+my_staging_bucket = f"gs://{config['AGENT_DEFAULT']['staging_bucket']}"
 
+vertexai.init(
+    project=my_project, 
+    location=my_location, 
+    staging_bucket=my_staging_bucket)
+     
 simple_agentic_flow = agent_structure.WorkflowManager(agents=agent_holder.agents)
 simple_agentic_flow.set_up()
 
 message = "What is the weather in Frances?"
 
 remote_agent = reasoning_engines.ReasoningEngine.create(
-    simple_agentic_flow(agents=agent_holder.agents),
+    agent_structure.WorkflowManager(agents=agent_holder.agents),
     requirements=[
         "google-cloud-aiplatform[langchain,reasoningengine]",
         "cloudpickle==3.0.0",
@@ -25,8 +30,11 @@ remote_agent = reasoning_engines.ReasoningEngine.create(
         "langgraph",
         "httpx",
     ],
-    display_name="Example MultiAgent Reasoning Engine with LangGraph",
+    display_name="SimpleAgentFlow",
     description="This is demo code",
-    extra_packages=[],)
+    extra_packages=[
+        "agents/",
+        "tools/", 
+        "utils/"],)
 
-print(remote_agent.query(question=message, user='hugo'))
+print(simple_agentic_flow.query(question=message, user='hugo'))
